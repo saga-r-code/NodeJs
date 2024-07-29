61a16840cd255b3a4b342284a110df2b9c054daa
saga-r-code <shah.sagar2006@gmail.com>
2024-07-30 03:32:00 +0530

    gym form backend and fronted join

--- Express/app.js
+++ Express/app.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const path = require('path')
+const fs = require('fs')
+
 const app = express()
 const port = 80
 
 //Express Specific Stuff
 app.use('/static', express.static('static'))//for the static file serve and show in text
-
+app.use(express.urlencoded({extended: true}))  //Middleware to parse URL-encoded data
 
 //Pug Reated Stuff
 app.set('view engine', 'pug')//set the templete engine for pug
@@ -15,26 +17,44 @@ app.set('views', path.join(__dirname, 'views'))//set the views directory
 
 //Endpoint with pug example
 app.get("/demo", (req, res)=>{
-    res.status(200).render("demo", {title: "Hey I am Pug", message: "Hello broo i am pug bro"})//our pug demo
+    //our pug demo
+    res.status(200).render("demo", {title: "Hey I am Pug", message: "Hello broo i am pug bro"})
 })
 
 app.get("/", (req,res)=>{
-    heading = "This is my first website using PUG"
-    param = {"title": "PUG", "heading" : heading}
+    const heading = "Get This Gym MemberShip for 60$ -Fill this form now!"
+    const param = {"title": "PUG", "heading" : heading}
+    res.status(200).render("index.pug", param)
+})
+
+app.post('/', (req, res)=>{
+    const object={
+     user : req.body.name,
+     age : req.body.age,
+     gender : req.body.gender,
+     address : req.body.address,
+     details : req.body.moreDetails,
+    }
+
+    const userDetails = JSON.stringify(object)
+
+    fs.writeFileSync('output.json', userDetails)
+    res.status(200).send("form submit successfully")
+    const param = {'message': 'Your Form Submited Successfully'}
     res.status(200).render("index.pug", param)
 })
 
 //express Endpoint 
-app.post("/", (req, res)=>{
+app.get("/", (req, res)=>{
     res.send("This is Home page of Express App ")
 })
 app.get("/about", (req, res)=>{
-    res.status(300).send("This is about page of Express App ")
+    res.status(300).send("This is about page of Express App  and use in pug")
 })
-app.post("/about", (req, res)=>{
+app.post("/", (req, res)=>{
     res.status(200).send("This is post request of about page of Express App ")
 })
-app.get("/home", (req, res)=>{
+app.get("/help", (req, res)=>{
     res.status(404).send("This page not found broo ")
 })
 
@@ -42,6 +62,8 @@ app.get("/home", (req, res)=>{
 
 
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`)
-})
+// app.listen(port, ()=>{
+//     console.log(`Server is running on port ${port}`)
+// })
+
+app.listen(port, () => console.log(`server is running on ${port}`))
