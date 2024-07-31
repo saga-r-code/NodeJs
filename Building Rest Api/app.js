f19b6821688d98e862c6ab38a3c64f3b0a8ccb7d
saga-r-code <shah.sagar2006@gmail.com>
2024-08-01 01:47:18 +0530

    rest api complete

--- Building Rest Api/app.js
+++ Building Rest Api/app.js
@@ -35,23 +35,60 @@ app.post("/api/user", (req, res) => {
 app.delete("/api/user/:id", (req, res) => {
   //Delete user with id
   const id = Number(req.params.id);
-  const user = users.find((user) => user.id === id); //from import
+  const user = users.findIndex((user) => user.id === id); //from import user and find index of the user
 
-  if (user !== -1) {
+  if (!user) {
+    // User not found
+    return res.status(404).json({ status: "Error", message: "User not found" });
+  } else {
     // Remove the user from the array
     users.splice(user, 1);
-    
+
     // Write the updated users array back to the JSON file
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users, null, 2), (err) => {
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users, null, 2), (err) => {
       if (err) {
-        return res.status(500).json({ status: 'Error', message: 'Failed to delete user' });
+        return res
+          .status(500)
+          .json({ status: "Error", message: "Failed to delete user" });
       }
-      return res.status(200).json({ status: 'Delete', id: id });
+      return res.status(200).json({ status: "Delete", id: id });
     });
-  } else {
-    // User not found
-    return res.status(404).json({ status: 'Error', message: 'User not found' });
   }
+});
+
+app.patch("/api/user/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.findIndex((user) => user.id === id);
+
+  if (!user) {
+    // User not found
+    return res.status(404).json({ status: "Error", message: "User not found" });
+  }
+
+  // Extract details to be updated from request body
+  const { first_name, last_name, email } = req.body; //get details for the update
+
+  if (first_name) {
+    users[user].first_name = first_name;
+  }
+  if (last_name) {
+    users[user].last_name = last_name;
+  }
+  if (email) {
+    users[user].email = email;
+  }
+  
+  // Write the updated users array back to the JSON file
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users, null, 2), (err) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ status: "Error", message: "Failed to updatelete user" });
+    }
+    return res
+      .status(200)
+      .json({ status: "successfully Update", id: id, user: users[user] });
+  });
 });
 
 app.listen(port, (req, res) => {
